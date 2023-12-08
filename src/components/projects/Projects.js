6bf4f82da90c8e22a087c228bc213e84b5037cdb
Tobi Wallace <twallace@catalyte.io>
2023-12-08 10:10:30 -0600

    add mobile compatability

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,33 @@
+import "./Projects.css";
+
 function Projects() {
   return (
     <div id="projects-page" className="section">
-      <p>a list of projects i want to highlight here</p>
+      <div className="right-section">
+        <h2>Work</h2>
+        <p>Catalyte</p>
+        <p>2021 - Present</p>
+        <ul>
+          <li>Brown and Caldwell</li>
+          <li>National Housing Compliance</li>
+          <li>First Western</li>
+          <li>3DE Schools</li>
+        </ul>
+      </div>
+      <div className="left-section">
+        <h2>Personal Projects</h2>
+        <p>a list of projects i want to highlight here</p>
+        <p>
+          Not sure if I'm going to actually use list format for this, probably
+          not
+        </p>
+        <ul>
+          <li>Fake storefront</li>
+          <li>PYTHON PROJECT</li>
+          <li>RSS Website</li>
+          <li>Snake</li>
+        </ul>
+      </div>
     </div>
   );
 }
